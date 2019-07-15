433584bfbe39bc7bc5a6b2c39302dea59c2fec53
Na Hyung Choi and Uriel Gonzalez <na@>
2019-07-15 14:54:50 -0700

    output result successfully

--- project.cs
+++ project.cs
@@ -1,9 +1,29 @@
 using System;
 
-class ProjectName
+class ClockAngle
 {
   static void Main()
   {
-    
+    Console.WriteLine("What's the hour?");
+    int hourInt = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("What's the minute?");
+    int minInt = int.Parse(Console.ReadLine());
+
+    float result = determineDistance(hourInt, minInt);
+
+    Console.WriteLine("The distance in degrees between the minute and hoir is: " + result + " degress!");
+  }
+
+  static float determineDistance(int hour, int minute)
+  {
+    float minDegrees = minute * 6;
+    float hourDegrees = Convert.ToSingle(hour * 30 + .5 * minute);
+    float diffAngle = Math.Abs(minDegrees - hourDegrees);
+    if (diffAngle > 180)
+    {
+      diffAngle = 360 - diffAngle;
+    }
+    return diffAngle;
   }
 }
